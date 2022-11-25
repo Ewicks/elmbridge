5032a74212b7994186bdede12454a6d44b2089ba
Ewicks <ethanowicks9@gmail.com>
2022-11-25 17:08:59 +0000

    add

--- tasks/views.py
+++ tasks/views.py
@@ -139,12 +139,9 @@ def code(x, wordlist):
 
 	baseurl = 'https://emaps.elmbridge.gov.uk/ebc_planning.aspx'
 
-	header = {
-		'User-agent':
-		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582'
-	}
+	
 
-	# header = {'User-Agent': user_agent}
+	header = {'User-Agent': user_agent}
 
 	b = True
 
